3087830bfb0e9c56c651a00ebd0d9f0f395b7281
Reed Nightingale <reed.nightingale@gmail.com>
2020-01-21 21:21:50 -0800

    Expose single letter morse to other code areas

--- morse.h
+++ morse.h
@@ -1,3 +1,4 @@
 #include "settings.h"
 //sends out morse code at the speed set by cwSpeed
+void morseLetter(char c, uint16_t dit_duration_ms = globalSettings.cwDitDurationMs);
 void morseText(char *text, uint16_t dit_duration_ms = globalSettings.cwDitDurationMs);
