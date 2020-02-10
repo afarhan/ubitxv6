51288f1db3fbe0176903bab6db1a1f5dac04e249
Reed Nightingale <reed.nightingale@gmail.com>
2020-02-10 00:35:32 -0800

    Define types for formatFreq

--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #define LIMIT(val,min,max) ((val) < (min)) ? (min) : (((max) < (val)) ? (max) : (val))
 
 void formatFreq(uint32_t freq, char* buff_out, uint16_t buff_size, uint8_t fixed_width = 0);
