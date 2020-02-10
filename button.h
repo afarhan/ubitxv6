bc1e546ece9dfc5fa8dbef8e4e3ff54393ef9b4b
Reed Nightingale <reed.nightingale@gmail.com>
2020-02-09 22:27:49 -0800

    const the size

--- button.h
+++ button.h
@@ -11,7 +11,7 @@ enum ButtonStatus_e : uint8_t {
 struct Button {
   int16_t x, y, w, h;
   const char* text;//nullptr if text_override should be used
-  void (*text_override)(char* text_out, uint16_t max_text_size);//nullptr if text should be used
+  void (*text_override)(char* text_out, const uint16_t max_text_size);//nullptr if text should be used
   ButtonStatus_e (*status)();//Used for coloring and morse menu
   void (*on_select)();//Action to take when selected
   char morse;
