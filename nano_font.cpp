98e62a6dcab76667e03ad3a438ebf5b8769bfbff
Reed Nightingale <reed.nightingale@gmail.com>
2020-04-21 21:40:02 -0700

    Switch to using a tiny but terrible looking font for size reasons

--- nano_font.cpp
+++ nano_font.cpp
@@ -1,6 +1,8 @@
 #include "nano_font.h"
 #include <avr/pgmspace.h>
 
-#include "PDQ_MinLib/FreeSansBold9pt7b.h"
+//#include "PDQ_MinLib/FreeSansBold9pt7b.h"
+#include "PDQ_MinLib/Picopixel.h"
 
-const GFXfont* ubitx_font = &FreeSansBold9pt7b;
+//const GFXfont* ubitx_font = &FreeSansBold9pt7b;
+const GFXfont* ubitx_font = &Picopixel;
--- nano_gui.cpp
+++ nano_gui.cpp
@@ -34,7 +34,7 @@ void displayInit(void){
   tft.setFont(ubitx_font);
   tft.setTextWrap(true);
   tft.setTextColor(DISPLAY_GREEN,DISPLAY_BLACK);
-  tft.setTextSize(1);
+  tft.setTextSize(4);
   tft.setRotation(1);
 }
 
