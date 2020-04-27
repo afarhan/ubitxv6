cd3364c835c0d52ecd720f97d0dd05fa750bc728
Reed Nightingale <reed.nightingale@gmail.com>
2020-04-26 23:36:06 -0700

    Update version

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
 #include "version.h"
 
-const char VERSION_STRING_PRIVATE [] PROGMEM = "R1.1.2";
+const char VERSION_STRING_PRIVATE [] PROGMEM = "R1.2.0";
 const char* const VERSION_STRING = VERSION_STRING_PRIVATE;
