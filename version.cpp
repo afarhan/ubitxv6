84f47b0a23986d69e32b0c8136223c82be1eda3c
Reed Nightingale <reed.nightingale@gmail.com>
2020-05-05 21:41:00 -0700

    Bump version

--- version.cpp
+++ version.cpp
@@ -1,4 +1,4 @@
 #include "version.h"
 
-const char VERSION_STRING_PRIVATE [] PROGMEM = "R1.4.0";
+const char VERSION_STRING_PRIVATE [] PROGMEM = "R1.5.0";
 const char* const VERSION_STRING = VERSION_STRING_PRIVATE;
