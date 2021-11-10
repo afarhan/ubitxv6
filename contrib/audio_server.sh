6ed808fc02eb10300790cb103dd7a9569f3f3835
Rafael Diniz <rafael@riseup.net>
2021-11-10 10:57:34 +0300

    add scripts for audio streaming...

---
+++ contrib/audio_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while true;
+do
+  ffmpeg  -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f webm -c:a libopus -b:a 16000 -listen 1 -seekable 0 -multiple_requests 1 http://127.0.0.1:9999
+done
+
+# serve with
+# <audio src="http://<host_ip>:9999" type="audio/webm" controls />
