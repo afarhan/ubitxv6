99a14ad45df3f3f59853626b17f34e2ae4586e3a
Rafael Diniz <rafael@riseup.net>
2021-11-10 12:46:07 +0300

    use mp3 in ts for udp

--- contrib/audio_udp_send.sh
+++ contrib/audio_udp_send.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ffmpeg -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f webm -c:a libopus -b:a 16000 udp://10.8.0.5:9999
+# ffmpeg -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f webm -c:a libopus -b:a 16000 udp://10.8.0.5:9999
 
-#ffmpeg -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f mpegts -c:a libmp3lame udp://10.8.0.5:9999
+ffmpeg -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f mpegts -c:a libmp3lame -b:a 16000 udp://10.8.0.5:9999
 
