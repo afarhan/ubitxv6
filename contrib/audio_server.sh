958df859f5b89b2d51f077eb0ce3e2a58fd9d14c
Rafael Diniz <rafael@riseup.net>
2021-11-10 11:04:01 +0300

    audio server works with html5 audio tag

--- contrib/audio_server.sh
+++ contrib/audio_server.sh
@@ -2,7 +2,7 @@
 
 while true;
 do
-  ffmpeg  -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f webm -c:a libopus -b:a 16000 -listen 1 -seekable 0 -multiple_requests 1 http://127.0.0.1:9999
+  ffmpeg  -f alsa -channels 1 -sample_rate 8000   -i plughw:0 -f webm -c:a libopus -b:a 16000 -listen 1 -seekable 0 -multiple_requests 1 http://0.0.0.0:9999
 done
 
 # serve with
